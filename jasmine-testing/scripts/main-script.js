971154c3b30a9118ce1ebff62b2ac10dced1074b
Ewan Lockwood <ewanlockwood4@icloud.com>
2019-05-01 15:24:12 +0000

    Jasmine Testing for Methods Function

--- jasmine-testing/scripts/main-script.js
+++ jasmine-testing/scripts/main-script.js
@@ -1,6 +1,6 @@
 var methods = function(error,data){
     jqueryFunctions(data);
-    // show_discipline_selector();
+    show_discipline_selector();
     show_country_selector();
     show_occurence_num();
     show_total_death_num();
@@ -11,39 +11,6 @@ var methods = function(error,data){
 };
 
 function jqueryFunctions(data) {
-    $(document).ready(function() {
-      
-      // Display Modal
-      $('#ModalCenter').modal('show');
-      
-      // Menu Functions
-      $("#close-btn").click(function(){
-        $("#reset-btn").animate({opacity: "0"}, 250).css("display", "none");
-        $("#disciplineSelector").animate({opacity: "0"}, 250).css("display", "none");
-        $("#countrySelector").animate({opacity: "0"}, 250).css("display", "none");
-        $(".menu").animate({height: '60px'}, 500);
-        $(".menu").css("background-color", "rgba(0,0,0,0")
-        $(".toggle-btn").addClass("row_one", 500);
-        $("#close-btn").css("display", "none");
-        $("#open-btn").css("display", "block").css("color", "rgb(57,59,121)");
-      });  
-      $("#open-btn").click(function(){
-        $("#reset-btn").animate({opacity: "100"}, 250).css("display", "block");
-        $("#disciplineSelector").animate({opacity: "100"}, 250).css("display", "block");
-        $("#countrySelector").animate({opacity: "110"}, 250).css("display", "block");
-        $(".menu").animate({height: '150px'}, 500).css("background-color", "rgba(57,59,121,1)");
-        $(".toggle-btn").removeClass("row_one", 500);
-        $("#close-btn").css("display", "block");
-        $("#open-btn").css("display", "none");
-      }); 
-      
-      $("#open-btn").hover(
-        function() {
-          $(this).animate({font: "200px"})
-        }), function() {
-          $(this).animate({color: "blue"})
-        }
-    })
   }
 function show_discipline_selector() {
     var test = 1
